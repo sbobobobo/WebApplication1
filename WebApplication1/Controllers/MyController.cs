ee5058b5e1d0d915f8056685b95836d3c5f7855c
Administrator <Administrator@6-206-A21>
2019-05-16 11:22:23 +0800

    添加项目文件。

---
+++ WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            ViewBag.bag = "this is a test!";
+            return View();
+        }
+    }
+}
